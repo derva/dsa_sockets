510203e2aedae986dbb1442371c3006963128064
derva <dervishalilcevic@gmail.com>
2022-11-25 20:57:45 +0100

    Add '.txt' extension

--- server.c
+++ server.c
@@ -11,11 +11,12 @@ int main(int argc , char *argv[])
     char client_message[2000];
      
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
+    if (socket_desc == -1) {
         printf("Could not create socket");
     }
+
     puts("Socket created");
+
     server.sin_family = AF_INET;
     server.sin_port = htons(8888);
     server.sin_addr.s_addr = INADDR_ANY;
@@ -46,59 +47,53 @@ int main(int argc , char *argv[])
                 puts("Client disconnected");
                 fflush(stdout);
             } else if(read_size == -1) {
-                perror("recv failed");
+                perror("Recieve failed");
             }
-            while( read_size = recv(client_sock , client_message , 2000 , 0)   )
+            while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0   )
             {
-                client_message[read_size]=0x00;
-                // printf("Kraj client poruke: %s ", client_message[read_size]);
-                // printf("Client poruka je: %s ", client_message);
-                int lenghtOfDataHeader = client_message[0] - '0';
-                // printf("lenghtOfData: %i ", lenghtOfDataHeader);
-                int i = 1;
                 char data[2000];
+                char finalData[2000];
+                printf("Data prije %s", data);
+                printf("Read size %i", read_size);
+                client_message[read_size]=0x00;
+                int lenghtOfDataHeader = client_message[0] - '0';
+                int i = 1;
+                printf("Length of data %i", lenghtOfDataHeader);
 
                 while ( i <= lenghtOfDataHeader ) {
                     data[i-1] = client_message[i];
-                    // printf("+");
                     i++;
                 }
+                printf("data poslije %s\n", data);
 
-                FILE* fp = fopen(data, "w");
+                //FILE* fp = fopen("/home/dervis/Documents/fileZaTest.txt", "w");
+                sprintf(finalData, "%s.txt", data);
+                printf("Data.txt: %s ", finalData);
+                FILE* fp = fopen(finalData, "w");
                 printf("\n=========\n");
                 printf("Zaglavlje: %s ", data);
                 int j = 0;
-                while ( client_message[i] != NULL ){
-                    // printf("-");
+                while ( client_message[i] != 0x00 ){
                     data[j] = client_message[i];
                     j++; 
                     i++;
                 }
-                data[j] = '\n';
                 if( fp == NULL ) {
                     printf("Neuspjesno otvaranje fajla");
                 } else{ 
+                    // printf("Upiso u fajl");
                     fprintf(fp, data);
                 }
                 fclose(fp);
                 printf("\n=========\n");
                 printf("Data: %s ", data);
                 printf("\n=========\n");
-                printf("client_message: %s ", client_message);
-                printf("strlen client message&: %li ", strlen(client_message));
-
-                printf("Server get: ");
-                // client_message[read_size]=0x00;
-                printf("%c\n", client_message[0]);
                 send(client_sock , client_message , strlen(client_message),0);
+                for ( int o = 0; o < read_size; o++ ){
+                    client_message[o] = '\0';
+                    data[o] = '\0';
+                }         
             }
-            
-            // if(read_size == 0){
-            //     puts("Client disconnected");
-            //     fflush(stdout);
-            // } else if(read_size == -1) {
-            //     perror("recv failed");
-            // }
         }
     }
     return 0;
